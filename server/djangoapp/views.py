d340624aad0db8c78f9d1675b12b3da9920c9ad4
chriscwelch <chris@ccwelch.co.uk>
2021-09-17 11:21:29 +0100

    model and view update for dealerships

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+from .models import CarMake, CarModel, CarDealer
+from .restapis import get_request, get_dealers_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -131,9 +131,30 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
+    """
+    Function to return delearship information
+    """
     context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+        url = 'https://141b0828.eu-gb.apigw.appdomain.cloud/api/dealership'
+
+        try:
+
+            # Get dealers from the URL
+            dealerships = get_dealers_from_cf(url)
+            print('dealerships from get_dealership view:', dealerships)
+
+            # Concat all dealers' short name
+            dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+
+            # Return a list of dealer short name
+            return HttpResponse(dealer_names)
+        
+        except Exception as error:
+            print("Error:", error)
+            return render(request, 'djangoapp/index.html', context)
+
+
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
